2c221c6f4480a11a1bffaeaf8776853396418c56
Daniel Schmid <yoyofreakcj@gmail.com>
2025-05-21 19:31:39 +0200

    Added filtered images information.

--- src/pages/download/Controls/Footer/Footer.tsx
+++ src/pages/download/Controls/Footer/Footer.tsx
@@ -2,18 +2,29 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { useTheme } from '@mui/material';
+import { useImages } from '../../Contexts/ImagesContext';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 
 const Footer: React.FC = () => {
   const theme = useTheme();
 
+  const images = useImages();
+
   return <Box component="footer" sx={{
-    p: 2,
     flex: '0 0 auto',
     borderTop: `2px solid ${theme.palette.divider}`
   }}>
-    <Typography variant="body2" align="center">
-      © {new Date().getFullYear()} Meine Web-App. Alle Rechte vorbehalten.
-    </Typography>
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Box
+        sx={{ display: 'inline-flex', alignItems: 'center', cursor: 'pointer' }}
+        title={`${images.images.length - images.filteredImages.length} images are hidden based on the current filters. Adjust the filters in the settings to show more or less images.`}
+      >
+        <HelpOutlineIcon fontSize="small" color="primary" sx={{ m: 1 }} />
+      </Box>
+      <Typography variant="caption" color="text.secondary" sx={{ fontStyle: 'italic', marginRight: 1 }}>
+        Showing {images.filteredImages.length} of {images.images.length} images
+      </Typography>
+    </Box>
   </Box>;
 };
 
